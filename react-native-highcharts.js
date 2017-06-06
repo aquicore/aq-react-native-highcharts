d70ba4b1b6f0cc69f4aec900a4fdd8114cbd6fdf
cantide5ga <cantide5ga@users.sourceforge.net>
2017-06-06 16:55:45 -0400

    AQ-00 +highchart extended functionalities

--- react-native-highcharts.js
+++ react-native-highcharts.js
@@ -15,6 +15,7 @@ class ChartWeb extends Component {
     return `<html>        
         <head>
           <style media="screen" type="text/css">#container{width:100%;height:100%;top:0;left:0;right:0;bottom:0;position:absolute;}</style>
+            <script src = "node_modules/highcharts-more/index.js">
             <script src="https://code.highcharts.com/${this.props.stock ? 'stock/highstock' : 'highcharts'}.js"></script>
             <script src="https://code.highcharts.com/modules/exporting.js"></script>
             <script>
